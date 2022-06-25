8216b0af45fda25691f16480eb44e980fb0e4831
ran yi <superyrrr@gmail.com>
2022-06-25 12:46:42 +0900

    result

---
+++ EL0306/Assets/IZEN/ResultManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ResultManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private string m_SinglePlaySceneName;
+    [SerializeField]
+    private string m_MultiPlaySceneName;
+    [SerializeField]
+    private string m_TitleSceneName;
+
+    [SerializeField]
+    private Text m_Score;
+
+    private bool m_GameMode;
+    [SerializeField]
+    private Image m_FloorImage;
+    [SerializeField]
+    private Image m_Floor2Image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_GameMode = false;
+        m_Floor2Image.enabled = false;
+
+        m_Score.text = "002";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            m_GameMode = false;
+            m_Floor2Image.enabled = false;
+            m_FloorImage.enabled = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            m_GameMode = true;
+            m_Floor2Image.enabled = true;
+            m_FloorImage.enabled = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (m_GameMode)
+            {
+                SceneManager.LoadScene(m_TitleSceneName);
+            }
+            else
+            {
+                SceneManager.LoadScene(m_MultiPlaySceneName);
+            }
+
+        }
+    }
+}
